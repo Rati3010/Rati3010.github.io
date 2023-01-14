0b73b07f37c52f2db3f5dc834d7ada2139ce0f93
Rati3010 <mratikanta1997@gmail.com>
2023-01-14 10:03:07 +0530

    added

--- src/components/GIthub.js
+++ src/components/GIthub.js
@@ -6,7 +6,7 @@ const Github = () => {
         <h1 style={{textAlign:"center",fontSize:"25px",marginBottom:"20px"}}>Github</h1>
         <ReactGitHubCalendar userName='Rati3010' responsive={true} />
         <h1 style={{textAlign:"center",fontSize:"25px",marginBottom:"20px"}}>Github Status</h1>
-        <p style={{margin:"auto"}}>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=Rati3010&show_icons=true&count_private=true" alt="rati3010" /></p>
+        <p style={{margin:"auto"}}>&nbsp;<img align="center" src="https://github-readme-stats-sigma-five.vercel.app/api?username=rati3010&show_icons=true&count_private=true&theme=react" alt="rati3010" /></p>
     </div>
   )
 }
